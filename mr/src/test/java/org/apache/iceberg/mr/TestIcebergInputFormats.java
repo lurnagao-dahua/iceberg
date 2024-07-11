951c1d5012f7b65de9ac35ea6404c651c3e70217
liu yang <91278331+lurnagao-dahua@users.noreply.github.com>
2024-07-11 16:41:00 +0800

    Update TestIcebergInputFormats.java

--- mr/src/test/java/org/apache/iceberg/mr/TestIcebergInputFormats.java
+++ mr/src/test/java/org/apache/iceberg/mr/TestIcebergInputFormats.java
@@ -425,12 +425,12 @@ public class TestIcebergInputFormats {
             () -> {
               try {
                 method.invoke(new IcebergInputFormat<>(), table, conf, workerpool);
-                return workerpool.
-                     submit(
+                return workerpool
+                    .submit(
                         () -> {
                           return UserGroupInformation.getCurrentUser().getUserName();
                         })
-                   .get();
+                    .get();
               } catch (Exception e) {
                 throw new RuntimeException("Failed to get user from worker pool", e);
               }
